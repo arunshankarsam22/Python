1d3d46315dc2a17a4ab3d8601de9f38a50a02282
arunshankarsam22 <52912428+arunshankarsam22@users.noreply.github.com>
2019-07-15 14:33:45 +0530

    Create posnegzero.py

---
+++ posnegzero.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a number:"))
+if(a>0):
+    print("The number is positive!")
+elif(a<0):
+    print("The number is negative!")
+elif(a==0):
+    print("The number is zero")
+else:
+    print("It's an invalid input")
