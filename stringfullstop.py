be100b76b19df2f65f521dafd99d850e0cc64f7b
arunshankarsam22 <52912428+arunshankarsam22@users.noreply.github.com>
2019-07-15 20:24:19 +0530

    Create stringfullstop.py

---
+++ stringfullstop.py
@@ -0,0 +1,9 @@
+'''Given a string S append a full stop (.) at the end of the string (use string stl).
+Sample Testcase :
+INPUT
+vishalrknagar
+OUTPUT
+vishalrknagar'''
+
+a=str(input())
+print(a + ("."))
